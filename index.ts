43ff034623789cdfad606f8a1b5a8f060a9c93d7
tmr08c <tmr08c@gmail.com>
2019-05-25 17:28:43 -0400

    Initial implementation of weekly summary logic.
    Pulls in weekly-summary package to get last 7 days of closed PRs and
    makes a big markdown string of them
    
    ENV Variables
    
    Updates `now.json` to set the ENV variable, `GITHUB_AUTH_TOKEN` (used by
    weekly-summary-typescript`) by leveraging the now "secret"
    `@github-auth-token`. For local `now dev` the secret is set through an
    `.env` file, so we also create a `.env.example` file to show the
    necessary key to set.

--- index.ts
+++ index.ts
@@ -1,3 +1,34 @@
-export default function (req, res) {
-  res.end(`Hello from TypeScript on Now 2.0!`);
-};
+import { IncomingMessage, ServerResponse } from "http";
+import { fetchRecentlyClosedPullRequests } from "weekly-summary-typescript";
+
+export default async function(req: IncomingMessage, res: ServerResponse) {
+  console.log("Running schedule for generating Weekly Summary");
+
+  console.log("Requesting Pull Requests");
+  const recentlyClosedPullRequests = await fetchRecentlyClosedPullRequests({
+    organization: "roirevolution"
+  })
+
+  console.log("Received Pull Requests. Generating e-mail.");
+
+  const emailBody = Object.entries(recentlyClosedPullRequests).reduce(
+    (emailBody, [repoName, pullRequests]) => {
+      emailBody += `# ${repoName}\n\n`;
+
+      pullRequests.forEach(pullRequest => {
+        emailBody += `* ${pullRequest.title} (${pullRequest.url})\n`;
+      });
+      emailBody += "\n";
+
+      return emailBody;
+    },
+    ""
+  );
+
+  console.log("Generated email body:");
+  console.log(emailBody);
+
+  console.log("Figure out how to send the email...");
+
+  res.end(emailBody);
+}
