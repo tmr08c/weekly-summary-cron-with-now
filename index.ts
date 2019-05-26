7637605b4c15663ee1383b8b4cb4a0f2769d0e7f
tmr08c <tmr08c@gmail.com>
2019-05-26 11:31:59 -0400

    Adds basic email sending.
    - Adds SendGrid dependency
    - Sets up environmental variables/now secrets

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { fetchRecentlyClosedPullRequests } from "weekly-summary-typescript";
+import * as sgMail from "@sendgrid/mail";
 
 export default async function(req: IncomingMessage, res: ServerResponse) {
   console.log("Running schedule for generating Weekly Summary");
@@ -7,7 +8,7 @@ export default async function(req: IncomingMessage, res: ServerResponse) {
   console.log("Requesting Pull Requests");
   const recentlyClosedPullRequests = await fetchRecentlyClosedPullRequests({
     organization: "roirevolution"
-  })
+  });
 
   console.log("Received Pull Requests. Generating e-mail.");
 
@@ -30,5 +31,20 @@ export default async function(req: IncomingMessage, res: ServerResponse) {
 
   console.log("Figure out how to send the email...");
 
+  sendEmail(emailBody);
+
   res.end(emailBody);
+}
+
+async function sendEmail(emailBody: string) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const email = {
+    to: "tmr08c@gmail.com",
+    from: "weekly-summary-cron@example.com",
+    subject: `Weekly Summary - ${new Date().toDateString()}`,
+    text: emailBody
+  };
+
+  await sgMail.send(email);
 }
