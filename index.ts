9676f211c564dc4276a3bbeed2c10b2f65a4b0fd
tmr08c <tmr08c@gmail.com>
2019-05-26 12:32:41 -0400

    Allows `to` to be passed in for sending email.
    Updates email sending to rely on `to` being passed in as a query parameter.

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { parse } from "url";
 import { IncomingMessage, ServerResponse } from "http";
 import { fetchRecentlyClosedPullRequests } from "weekly-summary-typescript";
 import * as sgMail from "@sendgrid/mail";
@@ -5,7 +6,15 @@ import * as sgMail from "@sendgrid/mail";
 export default async function(req: IncomingMessage, res: ServerResponse) {
   console.log("Running schedule for generating Weekly Summary");
 
+  const queryData = parse(req.url, true).query;
+  console.log(queryData);
+  let to = queryData.to || "";
+  if (Array.isArray(to)) {
+    to = to.join(",");
+  }
+
   console.log("Requesting Pull Requests");
+
   const recentlyClosedPullRequests = await fetchRecentlyClosedPullRequests({
     organization: "roirevolution"
   });
@@ -26,25 +35,37 @@ export default async function(req: IncomingMessage, res: ServerResponse) {
     ""
   );
 
-  console.log("Generated email body:");
-  console.log(emailBody);
+  console.log("Generated email body");
 
-  console.log("Figure out how to send the email...");
-
-  sendEmail(emailBody);
+  const response = await sendEmail({ to: to, body: emailBody });
 
   res.end(emailBody);
 }
 
-async function sendEmail(emailBody: string) {
+async function sendEmail({ to, body }: { to: string; body: string }) {
+  if (!process.env.SENDGRID_API_KEY || to.length == 0) {
+    console.log(
+      "SendGrid is not set up, or no `to` addresses specified. " +
+        "Skipping sending email."
+    );
+    return;
+  }
+
+  console.log("Sending email");
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const email = {
-    to: "tmr08c@gmail.com",
+    to: to,
     from: "weekly-summary-cron@example.com",
     subject: `Weekly Summary - ${new Date().toDateString()}`,
-    text: emailBody
+    text: body
   };
 
-  await sgMail.send(email);
+  const emailResponse = await sgMail.send(email);
+
+  console.log(`Sent email. Response: `);
+  console.log(emailResponse);
+
+  return emailResponse;
 }
