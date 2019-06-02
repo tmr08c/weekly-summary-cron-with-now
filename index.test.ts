d1a47c44816098c1ca3a357e21f2a084d5c44a3e
tmr08c <tmr08c@gmail.com>
2019-06-01 21:51:47 -0400

    WIP - Settig up fake requests.

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,18 @@
-test("jest is setup", () => {
+import index from "./index";
+import { request, createServer } from "http";
+import listen = require("test-listen");
+
+test("jest is setup", async () => {
+  // const server = createServer((req, res) => res.end(1));
+  // let url = await listen(server);
+  const srv = createServer((req, res) => res.end("1"));
+  let url = await listen(srv);
+
+  await request(url, res => {
+    console.log(res);
+
+    expect(res.statusCode).toBe(200);
+  });
+
   expect(true).toBeTruthy();
 });
